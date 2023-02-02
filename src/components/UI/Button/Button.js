0b73f112460d0cbbb0e45afb5887d84161f7aab1
Jyddys <justus.sydanmaa@hotmail.com>
2023-02-02 09:32:54 +0200

    Changed courseInput.css into module

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -6,7 +6,7 @@ import styles from './Button.module.css';
 // import './Button.css';
 
 // const Button = styled.button`
-//   width: 100%;
+//   width: 100%;s
 //   font: inherit;
 //   padding: 0.5rem 1.5rem;
 //   border: 1px solid #8b005d;
